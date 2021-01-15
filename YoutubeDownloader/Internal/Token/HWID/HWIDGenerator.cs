5d9c7d6a73c49f91d8f2e87d9b264228ae01b3f2
Thomas Nürk <jojopaul88@gmail.com>
2021-01-15 12:00:06 +0100

    Added HWID Generator

---
+++ YoutubeDownloader/Internal/Token/HWID/HWIDGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace YoutubeDownloader.Internal.HWID
+{
+    static class HWIDGenerator
+    {
+        public static string UID { get; private set; }
+
+
+        static HWIDGenerator()
+        {
+            var volumeSerial = DiskId.GetDiskId();
+            var cpuId = CpuId.GetCpuId();
+            var windowsId = WindowsId.GetWindowsId();
+            UID = volumeSerial + cpuId + windowsId;
+        }
+    }
+}
